b942869e40aec7b503cb62c3e9bee5c78c90f584
Imran Khan <imranlove1997@gmail.com>
2021-12-04 14:17:33 +0530

    Complete

---
+++ block-BNaabo/code/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+var app = express();
+
+app.use((req, res, next) => {
+    next();
+});
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false}));
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/json', (req, res) => {
+    console.log(req.body);
+})
+
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+})
